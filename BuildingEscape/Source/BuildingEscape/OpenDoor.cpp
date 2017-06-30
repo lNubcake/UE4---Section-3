ee97b81620df1d4266970ccf7559629e0adbb0ca
DESKTOP-N6K1GOL\szapa <szapa@windowslive.com>
2017-06-30 19:49:53 +0200

    Door opening blueprinted

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -37,12 +37,13 @@ void UOpenDoor::BeginPlay()
 
 void UOpenDoor::OpenDoor()
 {
-	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	// Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	OnOpenRequest.Broadcast();
 }
 
 void UOpenDoor::CloseDoor()
 {
-	Owner->SetActorRotation(FRotator(0.0f,OpenAngle+90, 0.0f));
+	// Owner->SetActorRotation(FRotator(0.0f,OpenAngle+90, 0.0f));
 }
 
 
